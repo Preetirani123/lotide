81df496f8cbdadca5ae4dadb27b194422d7d9fb9
Preetirani123 <rani.preeti3333@gmail.com>
2021-02-02 01:27:36 +0000

    add head.js

--- head.js
+++ head.js
@@ -1,3 +1,8 @@
+
+let head = function(array) {
+  return array[0];
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
@@ -9,6 +14,8 @@ const assertEqual = function(actual, expected) {
   
 };
 
+
+
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
