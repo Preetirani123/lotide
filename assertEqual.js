43cf23b821d5209243b6e586f57f68ea7b4eef54
Preetirani123 <rani.preeti3333@gmail.com>
2021-02-02 01:15:25 +0000

    add implemented code

--- assertEqual.js
+++ assertEqual.js
@@ -20,11 +20,11 @@ assertEqual(1, 1);*/
 // FUNCTION IMPLEMENTATION by second method
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed:   ${actual} ` === `  ${expected}`);
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   }
   
   else {
-    console.log(`Assertion Failed:  ${actual} ` !== `  ${expected}`);
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
   
 };
