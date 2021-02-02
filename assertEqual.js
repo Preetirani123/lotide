3c153928aaefc0c13a41342a6da93d3f73271fb6
Preetirani123 <rani.preeti3333@gmail.com>
2021-02-02 00:53:54 +0000

    add implemented code

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+/*// FUNCTION IMPLEMENTATION by first method
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log("Assertion Passed: " + actual + "=== " + expected);
@@ -6,6 +6,25 @@ const assertEqual = function(actual, expected) {
   
   else {
     console.log("Assertion Failed: " + actual + "!== " + expected);
+  }
+  
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);*/
+
+
+
+
+// FUNCTION IMPLEMENTATION by second method
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:   ${actual} ` === `  ${expected}`);
+  }
+  
+  else {
+    console.log(`Assertion Failed:  ${actual} ` !== `  ${expected}`);
   }
   
 };
