84c9f2e9987ab8a692aead20cb4c5d8cac784998
Preetirani123 <rani.preeti3333@gmail.com>
2021-02-03 06:37:09 +0000

    flatten.js

---
+++ flatten.js
@@ -0,0 +1,21 @@
+let flatten = function(array) {
+  
+  let newArr = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        newArr.push(array[i][j]);
+      }
+    }
+    else {
+      newArr.push(array[i]);
+    }
+  }
+
+  return newArr;
+
+};
+
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
