dd36b102916ea1f4479b6843e08667a79d923d73
Preetirani123 <rani.preeti3333@gmail.com>
2021-02-08 21:24:46 +0000

    add export import file

--- head.js
+++ head.js
@@ -1,21 +1,10 @@
-
 let head = function(array) {
   return array[0];
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-  
-  else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-
+module.exports = head;
 
 
 // TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
